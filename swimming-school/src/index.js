423b67d1d4bd96485dcecddf76f3b4c88e0420ac
Matej Juric <juric.matej7@gmail.com>
2019-09-24 18:22:07 +0200

    Welcome screen.

--- swimming-school/src/index.js
+++ swimming-school/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LoginCont from './containers/LoginCont';
 import RegistrationCont from './containers/RegistrationCont';
+import WelcomePage from './components/WelcomePage';
 
 function Application() {
     return (
         <Router>
             <Route exact path='/login' component={LoginCont} />
             <Route exact path='/registration' component={RegistrationCont} />
+            <Route exact path='/welcome' component={WelcomePage} />
         </Router>
     );
 }
